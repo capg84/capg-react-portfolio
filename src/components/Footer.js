4541523f741fa446fa577e5d98ede010ccc8818b
capg84 <cyprian.gomes@live.co.uk>
2022-11-28 23:12:52 +0000

    separated navbar css file from home css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,28 @@ const Footer = () => {
       <div className="footer">
         <a
           href="https://www.linkedin.com/in/cyprian-gomes-314b4939/"
-          className="icon fa-lg fa fa-linkedin"
-        ></a>
+          className="icon fa-lg fa fa-linkedin footer-icon"
+        >
+          .
+        </a>
         <a
           href="https://github.com/capg84"
-          className="icon fa-lg fa fa-github"
-        ></a>
+          className="icon fa-lg fa fa-github footer-icon"
+        >
+          .
+        </a>
         <a
           href="mailto:cyprian.gomes@live.co.uk"
-          className="icon fa-regular fa-at"
-        ></a>
-        <a href="tel:+447545839711" className="icon fa-solid fa-phone"></a>
+          className="icon fa-regular fa-at footer-icon"
+        >
+          .
+        </a>
+        <a
+          href="tel:+447545839711"
+          className="icon fa-solid fa-phone footer-icon"
+        >
+          .
+        </a>
       </div>
       <div className="copyright">&copy; 2022 Cyprian Gomes</div>
     </footer>
