2106ef6a59d2e7ac39b37c4a6ace0e1f9131e473
capg84 <cyprian.gomes@live.co.uk>
2022-12-04 14:20:14 +0000

    added contact form and helpers.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,15 @@
 const Contact = () => {
   return (
     <div>
-      <h1> Contact</h1>
+      <h2> Contact</h2>
+      <form
+        action="https://formsubmit.co/cyprian.gomes@live.co.uk"
+        method="POST"
+      >
+        <input type="text" name="name" required />
+        <input type="email" name="email" required />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
